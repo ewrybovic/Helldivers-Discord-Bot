5536e9e980aa409a3819ab772242dbcbe4bb13a1
ewrybovic <rybovic.evan@gmail.com>
2024-04-21 21:39:48 -0700

    Added major order rewards (#4)

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -1,6 +1,8 @@
 import re
 
 from API.helldivers_2_client.models import Assignment2
+from API.helldivers_2_client.models import Reward2
+from API.helldivers_2_client.models import Task2
 from API.helldivers_2_client.models import Dispatch
 
 # Regex to remove markup tags
@@ -8,7 +10,12 @@ CLEANR = re.compile('<.*?>')
 
 def format_major_order(MO: Assignment2) -> str:
     briefing = clean_markups(MO.briefing)
-    return f'# MAJOR ORDER\n**Brief**: {briefing}\n**Description**: {MO.description}\n**Expiration**: {format_time(MO.expiration)}\n**Progress**: {MO.progress}'
+    expiration = format_time(MO.expiration)
+    rewards = parse_rewards(MO.reward)
+
+    order = f'# MAJOR ORDER\n**Brief**: {briefing}\n**Description**: {MO.description}\n**Rewards**: {rewards}\n**Expiration**: {expiration}\n**Progress**: {MO.progress}'
+
+    return order
 
 def format_dispatch(dispatch: Dispatch) -> str:
     message = clean_markups(dispatch.message)
@@ -19,3 +26,11 @@ def format_time(time) -> str:
 
 def clean_markups(string: str) -> str:
     return re.sub(CLEANR, '', string)
+
+def parse_rewards(reward: Reward2) -> str:
+    # Not sure whta all these values will be, assign the raw value
+    type = reward.type
+    if type == 1:
+        type = 'Medals'
+
+    return f'{reward.amount} {type}'
